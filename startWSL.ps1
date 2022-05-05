46b264d0b93fd0b510a7939bc6f22f2d1772ef8a
blue14753 <blue14753@gmail.com>
2022-05-05 15:42:22 +0800

    fix startWSL ubuntu to ubuntu_password

--- startWSL.ps1
+++ startWSL.ps1
@@ -2,5 +2,5 @@ $ubuntu_password=admin
 $activate_mac=Get-NetRoute -DestinationPrefix '0.0.0.0/0' | Get-NetAdapter
 $mac_address=$activate_mac.macaddress.replace('-',':')
 
-wsl echo $ubuntu `| sudo -S ip link set dev bond0 address $mac_address
+wsl echo $ubuntu_password `| sudo -S ip link set dev bond0 address $mac_address
 wsl -d Ubuntu-20.04
